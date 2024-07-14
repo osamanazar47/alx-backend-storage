474bd9aaa9bf8751d7123c55abe814e04ce71a2e
osamanazar47 <osamanazar47@gmail.com>
2024-07-14 10:49:06 +0000

    avg_score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -8,7 +8,7 @@ BEGIN
 	SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
 
 	-- Update the user's average score in the users table
-	UPDATE user SET average_score = avg_score WHERE id = user_id;
+	UPDATE users SET average_score = avg_score WHERE id = user_id;
 END //
 
 DELIMITER;
