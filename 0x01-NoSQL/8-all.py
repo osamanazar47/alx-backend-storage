79fe67f7d090f5c3080981eb96c1fb604a6f166c
osamanazar47 <osamanazar47@gmail.com>
2024-07-16 13:22:25 -0700

    list doc in pymongo task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""A function that lists all documents in a collection"""
+import pymongo
+
+
+def list_all(mongo_collection):
+    """returns a list of all documents"""
+    return [document for document in mongo_collection.find() 
