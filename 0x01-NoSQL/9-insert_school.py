de27ddc4109f889df53691d9759dded958ba54b8
osamanazar47 <osamanazar47@gmail.com>
2024-07-16 16:12:57 -0700

    insert doc

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -5,4 +5,4 @@
 def insert_school(mongo_collection, **kwargs):
     """function that insrets a new doc"""
     result = mongo_collection.insert_one(kwargs)
-    return result._id
+    return result.inserted_id
