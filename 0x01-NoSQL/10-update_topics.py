3f3128c597a36d2cb5285fb99c6ea951dcf670a3
osamanazar47 <osamanazar47@gmail.com>
2024-07-16 16:25:16 -0700

    update topics task 10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,1 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""module for task 10 updating a document"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """a function that changes all topics of a school document based on the name"""
+    mongo_collection.update_many({"name": name}, {$set: {"topics": topics}})
