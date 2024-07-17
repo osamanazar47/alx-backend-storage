36b564cc22e54e0a560c1496f89fc88b88702b1d
osamanazar47 <osamanazar47@gmail.com>
2024-07-18 00:30:54 +0300

    task 0 style

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,7 @@
 """Module for task 0(writing strings to redis)"""
 import redis
 import uuid
+from typing import Union
 
 
 class Cache:
@@ -11,7 +12,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
-    def store(self, data: str | bytes | int | float) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """takes a data argument and returns a string"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
