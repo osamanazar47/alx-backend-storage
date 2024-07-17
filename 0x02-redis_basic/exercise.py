919af23277d0db0057c36b554dab169d9c5df8bb
osamanazar47 <osamanazar47@gmail.com>
2024-07-18 00:27:03 +0300

    class Cache and method store

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,1 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Module for task 0(writing strings to redis)"""
+import redis
+import uuid
+
+
+class Cache:
+    """Cache class"""
+    def __init__(self):
+        """initialization of the Cache class"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: str | bytes | int | float) -> str:
+        """takes a data argument and returns a string"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
